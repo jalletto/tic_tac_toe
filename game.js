86470f70df4e0f53e58924d2da365dfb228a1c6c
Joshua Alletto <joshalletto@gmail.com>
2017-12-07 19:12:53 -0600

    fixed typo

--- game.js
+++ game.js
@@ -1,6 +1,6 @@
 // What is the game state?
 // when you create a constructor function, a prototype object gets made: Game.prototype = {}; now exists
-var TicTacToe = function(){
+let TicTacToe = function(){
   this.player1 = 'X'
   this.player2 = 'O'
   this.gameOver = false
@@ -22,9 +22,6 @@ var TicTacToe = function(){
   ]
 }
 
-TicTacToe.prototype.printBoard = function(){
-  console.log(this.board)
-};
 
 TicTacToe.prototype.updateBoard = function(e){
   if(this.board[e.target.id] === " " ){
@@ -39,47 +36,6 @@ TicTacToe.prototype.updateHTMLBoard = function(e){
 }
 
 
-TicTacToe.prototype.updateHTML = function(e){
-    e.target.innerHTML = game.activePlayer
-
-}
-
-// TicTacToe.prototype.verticalWin = function(){
-//   if(this.board[0] === this.activePlayer && this.board[3] === this.activePlayer && this.board[6] === this.activePlayer ){
-//     return true
-
-//   } else if(this.board[1] === this.activePlayer && this.board[4] === this.activePlayer && this.board[7] === this.activePlayer ){
-//      return true
-
-//   }else if(this.board[2] === this.activePlayer && this.board[5] === this.activePlayer && this.board[8] === this.activePlayer ){
-//      return true
-//   }
-//     return false
-// }
-
-// TicTacToe.prototype.horizontalWin = function(){
-//   if(this.board[0] === this.activePlayer && this.board[1] === this.activePlayer && this.board[2] === this.activePlayer ){
-//       return true
-
-//   } else if(this.board[3] === this.activePlayer && this.board[4] === this.activePlayer && this.board[5] === this.activePlayer ){
-//       return true
-
-//   }else if(this.board[6] === this.activePlayer && this.board[7] === this.activePlayer && this.board[8] === this.activePlayer ){
-//       return true
-//   }
-//     return false
-// }
-
-// TicTacToe.prototype.diagonalWin = function(){
-//   if(this.board[0] === this.activePlayer && this.board[4] === this.activePlayer && this.board[8] === this.activePlayer ){
-//       return true
-
-//   } else if(this.board[6] === this.activePlayer && this.board[4] === this.activePlayer && this.board[2] === this.activePlayer ){
-//       return true
-//     }
-//     return false
-// }
-
 TicTacToe.prototype.win = function(){
     for(let i = 0; i < this.winCombos.length; i++ ){
       if(this.board[this.winCombos[i][0]] === this.activePlayer && this.board[this.winCombos[i][1]] === this.activePlayer && this.board[this.winCombos[i][2]] === this.activePlayer){
@@ -87,7 +43,6 @@ TicTacToe.prototype.win = function(){
       }
     }
     return false
-
 }
 
 TicTacToe.prototype.updateHTMLCurrentPlayer = function(){
@@ -103,7 +58,6 @@ TicTacToe.prototype.switchActivePlayer = function(){
 
 TicTacToe.prototype.gameWon = function(){
   if(this.win()){
-
     this.gameOver = true
   }
   return this.gameOver
@@ -133,9 +87,7 @@ TicTacToe.prototype.run = function(){
   let table = document.querySelector('table')
   let game = this
   table.addEventListener("click", function(e){
-    game.printBoard()
     game.updateBoard(e)
-    game.printBoard()
     game.endGame()
     game.switchActivePlayer()
 })
